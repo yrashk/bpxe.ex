e44238d9488f805554d27d980bd1a19c144eaa79
Yurii Rashkovskii <yrashk@gmail.com>
2021-01-11 10:24:17 -0800

    Problem: shell.nix does not specify Erlang/Elixir versions
    This can lead to differences in development environment setups.
    
    Solution: use more specific vresions from `unstable` channel
    
    These versions are not precise but they are better than nothing

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { 
-  pkgs ? import <nixpkgs> {}
+  pkgs ? import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {}
 }:
 
 pkgs.stdenv.mkDerivation rec {
   name = "bpxe-shell";
 
-  buildInputs = with pkgs; [ elixir ];
+  buildInputs = with pkgs; [ beam.packages.erlangR23.elixir_1_11 ];
 
   shellHook = ''
     export ERL_LIBS=
