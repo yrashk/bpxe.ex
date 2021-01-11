7b8235409aaf6e57f8bc7c8ed8a25e42301e61bd
Yurii Rashkovskii <yrashk@gmail.com>
2021-01-11 10:34:18 -0800

    Problem: CI tests fail sporadically
    A good number of them fails because of expired timeouts for asserting
    received messages or refuting receipt. They fail much more rarely on my
    local setup.
    
    Solution: increase timeouts for CI

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
-ExUnit.configure(assert_receive_timeout: 500, refute_receive_timeout: 500)
+receive_timeout = if System.get_env("CI"), do: 5000, else: 500
+refute_receive_timeout = if System.get_env("CI"), do: 5000, else: 500
+ExUnit.configure(assert_receive_timeout: receive_timeout, refute_receive_timeout: refute_receive_timeout)
 ExUnit.start()
