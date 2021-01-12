6433fb69cb29b553d75ab519094d3247584fb649
Yurii Rashkovskii <yrashk@gmail.com>
2021-01-12 11:50:35 -0800

    Problem: mix format does not agree with some formatting
    Solution: let it win

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,9 @@
 receive_timeout = if System.get_env("CI"), do: 5000, else: 500
 refute_receive_timeout = if System.get_env("CI"), do: 5000, else: 500
-ExUnit.configure(assert_receive_timeout: receive_timeout, refute_receive_timeout: refute_receive_timeout)
+
+ExUnit.configure(
+  assert_receive_timeout: receive_timeout,
+  refute_receive_timeout: refute_receive_timeout
+)
+
 ExUnit.start()
